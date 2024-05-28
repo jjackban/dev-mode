04ed362e34450ac052ddda3da5f35fcf2ccbef52
jjackban <bjh0921@gmail.com>
2024-05-29 00:53:34 +0900

    feat: update scripts

--- basic-network/scripts/upNetwork.sh
+++ basic-network/scripts/upNetwork.sh
@@ -2,33 +2,22 @@
 
 cd $GOPATH/src/dev-mode/basic-network/docker
 
-## 파라미터가 없으면 종료
-if [ "$#" -lt 1 ]; then
-   echo "$# is Illegal number of parameters."
-   echo "Usage: $0 [options]"
- exit 1
-fi
-args=("$@")
-
 echo "노드 컨테이너 실행"
 
 export COMPOSE_PROJECT_NAME=fabric
 export IMAGE_TAG=2.2.2
 export SYS_CHANNEL=system-channel
 
-for (( c=0; c<$#; c++ ))
-do
- case ${args[$c]} in
-   org1peer0)
-     docker-compose -f docker-compose-test-net.yaml -f docker-compose-test-net.yaml up -d cli
-     ;;
-   orderer)
-     docker-compose -f docker-compose-test-net.yaml up -d orderer.example.com
-     ;;
-   *)
-     echo -n "unknown parameter"
-     ;;
- esac
-done
+if [ "$1" == "dev" ]; then
+  docker-compose -f docker-compose-test-net.yaml up -d cli
+  docker-compose -f docker-compose-test-net.yaml up -d orderer.example.com
+elif [ "$1" == "prod" ]; then
+  docker-compose -f docker-compose-prod.yaml up -d cli
+  docker-compose -f docker-compose-prod-org3.yaml up -d cli-org3
+  docker-compose -f docker-compose-prod.yaml up -d orderer.example.com orderer2.example.com
+else
+  echo -n "unknown parameter"
+  exit 1
+fi
 
 docker ps
--- network.sh
+++ network.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 function generateCert() {
- # 인증서 dev 모드 생성
- basic-network/scripts/generateCert.sh
+ # 인증서 생성
+ basic-network/scripts/generateCert.sh $1
 }
 
 function runCAdev() {
  # CA dev 생성
  basic-network/scripts/runCAdev.sh
+}
+
+function runCAOrg3() {
+ # CA org3 생성
+ basic-network/scripts/runCAOrg3.sh
 }
 
 function cleanNetwork() {
@@ -17,11 +22,11 @@ function cleanNetwork() {
 
 function upNetwork() {
  # 네트워크 실행
- basic-network/scripts/upNetwork.sh $1 $2 $3 $4 $5 $6
+ basic-network/scripts/upNetwork.sh $1
 }
 function createConfigtxgen() {
  # 채널 정보 생성
- basic-network/scripts/createConfigtxgen.sh
+ basic-network/scripts/createConfigtxgen.sh $1
 }
 
 function joinChannel() {
@@ -49,11 +54,11 @@ function upgradeCC() {
  docker exec cli scripts/upgradeCC.sh $1 $2
 }
 if [ "$1" == "generateCert" ]; then
- generateCert org1peer0 orderer
+ generateCert $2
 elif [ "$1" == "createConfigtxgen" ]; then
- createConfigtxgen
+ createConfigtxgen $2
 elif [ "$1" == "upNetwork" ]; then
- upNetwork org1peer0 orderer
+ upNetwork $2
 elif [ "$1" == "createChannel" ]; then
  joinChannel createChannel
 elif [ "$1" == "joinChannel" ]; then
@@ -66,18 +71,35 @@ elif [ "$1" == "checkCC" ]; then
  checkCC $2
 elif [ "$1" == "runCAdev" ]; then
  runCAdev
+elif [ "$1" == "runCAOrg3" ]; then
+ runCAOrg3
 elif [ "$1" == "startSDK" ]; then
  startSDK
 elif [ "$1" == "upgradeCC" ]; then
  upgradeCC $2 $3
 elif [ "$1" == "clean" ]; then
  cleanNetwork
-elif [ "$1" == "start" ]; then
- generateCert
+elif [ "$1" == "dev" ]; then
+ generateCert dev
  sleep 2
- createConfigtxgen
+ createConfigtxgen dev
  sleep 2
- upNetwork org1peer0 orderer
+ upNetwork dev
+ sleep 2
+ joinChannel createChannel
+ joinChannel joinChannel
+ joinChannel updateAnchor
+ sleep 2
+ runCAdev
+elif [ "$1" == "prod" ]; then
+ generateCert prod
+ sleep 2
+ runCAOrg3
+ sleep 2
+ createConfigtxgen prod
+ sleep 2
+ upNetwork prod
+ sleep 2
  sleep 2
  joinChannel createChannel
  joinChannel joinChannel
