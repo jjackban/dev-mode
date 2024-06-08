fe6494e307f925b83d89304e523f0f521e25c042
ParkJaeHwi <2606pjh@naver.com>
2024-06-08 16:42:34 +0900

    server.js수정

--- application/rest/server.js
+++ application/rest/server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/init', function (req, res) {
    let a = req.query.a;
    let aval = req.query.aval;
-   let args = [a, aval, b, bval, c, cval];
+   let args = [a, aval];
    sdk.send(false, 'init', args, res);
 });
 
@@ -19,6 +19,21 @@ app.get('/query', function (req, res) {
    let name = req.query.name;
    let args = [name];
    sdk.send(true, 'query', args, res);
+});
+
+app.get('/delete', function (req, res) {
+   let name = req.query.name;
+   let args = [name];
+   sdk.send(false, 'delete', args, res);
+});
+
+
+app.get('/invoke', function (req, res) {
+   let sender = req.query.sender;
+   let receiver = req.query.receiver;
+   let amount = req.query.amount;
+   let args = [sender, receiver, amount];
+   sdk.send(false, 'invoke', args, res);
 });
 
 app.use(express.static(path.join(__dirname, '../client')));
