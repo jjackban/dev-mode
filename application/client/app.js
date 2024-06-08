1b99c897dd0aee0af1be76b8f091456843d51ef1
BH_Ubuntu <gej99@bu.ac.kr>
2024-06-08 17:19:05 +0900

    success.xx hide n show

--- application/client/app.js
+++ application/client/app.js
@@ -2,51 +2,53 @@
 
 var app = angular.module('application', []);
 
-app.controller('AppCtrl', function($scope, appFactory){
-   $("#success_init").hide();
-   $("#success_qurey").hide();
-   $("#success_delete").hide();
+app.controller('AppCtrl', function ($scope, appFactory) {
+    $("#success_init").hide();
+    $("#success_qurey").hide();
+    $("#success_delete").hide();
 
-   $scope.initAB = function(){
-       appFactory.initAB($scope.abstore, function(data){
-           if(data == "")
-           $scope.init_ab = "success";
-           $("#success_init").show();
-       });
-   }
-   $scope.queryAB = function(){
-       appFactory.queryAB($scope.walletid, function(data){
-           $scope.query_ab = data;
-           $("#success_qurey").show();
-       });
-   }
-   $scope.deleteAB = function(){
-        appFactory.deleteAB($scope.deleteid, function(data){
+    $scope.initAB = function () {
+        $("#success_init").hide();
+        appFactory.initAB($scope.abstore, function (data) {
+            if (data == "")
+                $scope.init_ab = "success";
+            $("#success_init").show();
+        });
+    }
+    $scope.queryAB = function () {
+        $("#success_qurey").hide();
+        appFactory.queryAB($scope.walletid, function (data) {
+            $scope.query_ab = data;
+            $("#success_qurey").show();
+        });
+    }
+    $scope.deleteAB = function () {
+        $("#success_delete").hide();
+        appFactory.deleteAB($scope.deleteid, function (data) {
             $scope.delete_ab = "success";
             $("#success_delete").show();
         })
-   }
+    }
 });
-app.factory('appFactory', function($http){
-      
+app.factory('appFactory', function ($http) {
+
     var factory = {};
- 
-    factory.initAB = function(data, callback){
-        $http.get('/init?a='+data.a+'&aval='+data.aval).success(function(output){
+
+    factory.initAB = function (data, callback) {
+        $http.get('/init?a=' + data.a + '&aval=' + data.aval).success(function (output) {
             callback(output)
         });
     }
-    factory.queryAB = function(name, callback){
-        $http.get('/query?name='+name).success(function(output){
+    factory.queryAB = function (name, callback) {
+        $http.get('/query?name=' + name).success(function (output) {
             callback(output)
         });
     }
 
-    factory.deleteAB = function(name, callback){
-        $http.get('/delete?name='+name).success(function(output){
+    factory.deleteAB = function (name, callback) {
+        $http.get('/delete?name=' + name).success(function (output) {
             callback(output)
         });
     }
     return factory;
- });
- 
+});
