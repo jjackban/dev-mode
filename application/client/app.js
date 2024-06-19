489e51e5d00164e5a0bbe2c3638dfc9d240d7505
jjackban <bjh0921@gmail.com>
2024-06-20 00:05:14 +0900

    feat: add function

--- application/client/app.js
+++ application/client/app.js
@@ -5,7 +5,9 @@ var app = angular.module('application', []);
 app.controller('AppCtrl', function($scope, appFactory){
    $("#success_init").hide();
    $("#success_qurey").hide();
+   $("#success_delete").hide();
    $scope.initAB = function(){
+       $("#success_init").hide();
        appFactory.initAB($scope.abstore, function(data){
            if(data == "")
            $scope.init_ab = "success";
@@ -13,23 +15,37 @@ app.controller('AppCtrl', function($scope, appFactory){
        });
    }
    $scope.queryAB = function(){
+       $("#success_qurey").hide();
        appFactory.queryAB($scope.walletid, function(data){
            $scope.query_ab = data;
            $("#success_qurey").show();
        });
    }
+   $scope.deleteAB = function(){
+        $("#success_delete").hide();
+        appFactory.deleteAB($scope.deleteid, function(data){
+            if(data == "")
+            $scope.delete_ab = "success";
+            $("#success_delete").show();
+        });
+    }
 });
 app.factory('appFactory', function($http){
       
     var factory = {};
  
     factory.initAB = function(data, callback){
-        $http.get('/init?a='+data.a+'&aval='+data.aval+'&b='+data.b+'&bval='+data.bval+'&c='+data.c+'&cval='+data.cval).success(function(output){
+        $http.get('/init?user='+data.a+'&userVal='+data.aval+'&b='+data.b+'&bval='+data.bval+'&c='+data.c+'&cval='+data.cval).success(function(output){
             callback(output)
         });
     }
     factory.queryAB = function(name, callback){
         $http.get('/query?name='+name).success(function(output){
+            callback(output)
+        });
+    }
+    factory.deleteAB = function(name, callback){
+        $http.get('/delete?name='+name).success(function(output){
             callback(output)
         });
     }
