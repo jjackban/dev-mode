9dc305d8c433da9814f5e6aff1edad5e8a9c4fc5
hkyuuu00 <lhk3121@gmail.com>
2024-06-20 20:40:23 +0900

    edit server2

--- application/rest/server.js
+++ application/rest/server.js
@@ -11,19 +11,24 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/init', function (req, res) {
    let a = req.query.a;
    let aval = req.query.aval;
-   let b = req.query.b;
-   let bval = req.query.bval;
-   let c = req.query.c;
-   let cval = req.query.cval;
-   let args = [a, aval, b, bval, c, cval];
+   let args = [a, aval];
    sdk.send(false, 'init', args, res);
 });
 
-app.get('/query', function (req, res) {
-   let name = req.query.name;
-   let args = [name];
-   sdk.send(true, 'query', args, res);
-});
+app.get('/delete', function (req, res) {
+    let name = req.query.name;
+    let args = [name];
+    sdk.send(false, 'delete', args, res);
+ });
+ 
+ 
+ app.get('/invoke', function (req, res) {
+    let sender = req.query.sender;
+    let receiver = req.query.receiver;
+    let amount = req.query.amount;
+    let args = [sender, receiver, amount];
+    sdk.send(false, 'invoke', args, res);
+ });
 
 app.use(express.static(path.join(__dirname, '../client')));
 app.listen(PORT, HOST);
