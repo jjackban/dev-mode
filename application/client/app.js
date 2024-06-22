27c0e16276659370e9d85f4ac7034e32588aa8b7
jcentury <tprl0328@naver.com>
2024-06-22 15:10:52 +0900

    team2

--- application/client/app.js
+++ application/client/app.js
@@ -5,11 +5,10 @@ var app = angular.module('application', []);
 app.controller('AppCtrl', function($scope, appFactory){
    $("#success_init").hide();
    $("#success_invoke").hide();
-   $("#success_qurey").hide();
-   $("#success_qurey_admin").hide();
+   $("#success_qurer").hide();
+   $("#success_admin").hide();
    $("#success_delete").hide();
    $scope.initAB = function(){
-   $("#success_init").hide();
        appFactory.initAB($scope.abstore, function(data){
            if(data == "")
            $scope.init_ab = "success";
@@ -17,47 +16,43 @@ app.controller('AppCtrl', function($scope, appFactory){
        });
    }
    $scope.invokeAB = function(){
-       $("#success_invoke").hide();
-       appFactory.invokeAB($scope.invoke, function(data){
-           if(data == "")
-           $scope.invoke_ab = "success";
-           $("#success_invoke").show();
-       });
-   }
-   $scope.queryAB = function(){
-       $("#success_qurey").hide();
-       appFactory.queryAB($scope.walletid, function(data){
-           $scope.query_ab = data;
-           $("#success_qurey").show();
-       });
-   }
-   $scope.queryAdmin = function(){
-       $("#success_qurey_admin").hide();
-       appFactory.queryAB("admin", function(data){
-           $scope.query_admin = data;
-           $("#success_qurey_admin").show();
-       });
-   }
-   $scope.deleteAB = function(){
-        $("#success_delete").hide();
-        appFactory.deleteAB($scope.deleteid, function(data){
+        appFactory.invokeAB($scope.transfer, function(data){
             if(data == "")
-            $scope.delete_ab = "success";
-            $("#success_delete").show();
+            $scope.invoke_ab = "success";
+            $("#success_invoke").show();
         });
     }
+   $scope.queryAB = function(){
+       appFactory.queryAB($scope.walletid, function(data){
+           $scope.query_ab = data;
+           $("#success_query").show();
+       });
+   }
+   $scope.adminAB = function(){
+    appFactory.queryAB("admin", function(data){
+        $scope.admin_ab = data;
+        $("#success_admin").show();
+    });
+}
+   $scope.deleteAB = () => {
+    appFactory.deleteAb($scope.deleteId, (data) => {
+        if(data == "")
+        $scope.delete_ab = "success";
+        $("#success_delete").show();
+    });
+   }
 });
 app.factory('appFactory', function($http){
       
     var factory = {};
  
     factory.initAB = function(data, callback){
-        $http.get('/init?user='+data.a+'&userVal='+data.aval+'&b='+data.b+'&bval='+data.bval+'&c='+data.c+'&cval='+data.cval).success(function(output){
+        $http.get('/init?a='+data.a+'&aval='+data.aval).success(function(output){
             callback(output)
         });
     }
     factory.invokeAB = function(data, callback){
-        $http.get('/invoke?sender='+data.sender+'&receiver='+data.receiver+'&amount='+data.amount).success(function(output){
+        $http.get('/invoke?sender='+data.sender+'&reciever='+data.reciever+'&value='+data.value).success(function(output){
             callback(output)
         });
     }
@@ -66,11 +61,13 @@ app.factory('appFactory', function($http){
             callback(output)
         });
     }
-    factory.deleteAB = function(name, callback){
-        $http.get('/delete?name='+name).success(function(output){
+    factory.deleteAb = (name, callback) => {
+        $http.get('/delete?name='+name).success((output) => {
             callback(output)
-        });
-    }
+        })
+    } 
+
+
     return factory;
  });
  
